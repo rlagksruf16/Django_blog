823ea8747acd778a199b1c3433bf2d2d7ba542ea
rlagksruf16 <rlagksruf16@gmail.com>
2019-07-30 23:24:56 +0900

    second commit

--- postproject/settings.py
+++ postproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +137,8 @@ TEMPLATES = [
         },
     },
 ]
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
